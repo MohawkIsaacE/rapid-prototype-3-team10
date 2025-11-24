b865af20052582031729b7d4c22f01fd29c34c58
Isaac <isaac.ebert@mohawkcollege.ca>
2025-11-24 12:52:48 -0500

    Add cue ball hitting

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [field: SerializeField]
+    public Transform Target { get; private set; }
+
+    [field: SerializeField]
+    public float XOffset { get; private set; }
+
+    [field: SerializeField]
+    public float YOffset { get; private set; }
+
+    [field: SerializeField]
+    public float ZOffset { get; private set; }
+
+    [field: SerializeField, Range(0f, 1f)]
+    public float LookAtStrength { get; private set; } = 1f;
+
+
+    void Update()
+    {
+        SetPosition();
+        LookAtTarget();
+    }
+
+    private void OnValidate()
+    {
+        float positionZ = ComputePositionZ();
+        bool isIncorrectPosition =
+            this.transform.position.x != XOffset ||
+            this.transform.position.y != YOffset ||
+            this.transform.position.z != positionZ;
+        if (isIncorrectPosition)
+        {
+            SetPosition();
+            LookAtTarget();
+        }
+    }
+
+    private float ComputePositionZ()
+    {
+        float positionZ = Target.position.z + ZOffset;
+        return positionZ;
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 position = Target.position;
+        position.y = Mathf.Lerp(0, position.y, LookAtStrength);
+        transform.LookAt(position);
+    }
+
+    private void SetPosition()
+    {
+        Vector3 position = new Vector3();
+        position.x = XOffset;
+        position.y = YOffset;
+        position.z = ComputePositionZ();
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/CueBallHit.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody CueBallrb;
+    public float HitForceKPH = 1;
+    public bool DoCueBallHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoCueBallHit = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!DoCueBallHit)
+            return;
+        DoCueBallHit = false;
+
+        Vector3 hitDirection = CueBallrb.transform.forward;
+        Vector3 force = HitForceKPH * CueBallrb.mass * hitDirection;
+        CueBallrb.AddForce(force, ForceMode.Impulse);
+
+        //Vector3 kickSpinDirection = CueBallrb.transform.up;
+        //Vector3 torque = kickSpinDirection * KickSpinForce;
+        //CueBallrb.AddTorque(torque, ForceMode.Impulse);
+    }
+}
